fe86ab0761fc803faa813c0bd3a0b125992dd56f
DancheBacheva <danchebacheva@gmail.com>
2024-09-17 20:09:46 +0200

    RecipeDetailView

--- Baby's-Food-Place/src/pages/MainPage/MainPage.jsx
+++ Baby's-Food-Place/src/pages/MainPage/MainPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./MainPage.module.css";
 import { Title } from "../../components/Title/Title";
 import { FreshAndNew } from "../../components/Fresh&New/FreshAndNew";
+import { RecipeDetailView } from "../../components/RecipeDetailView/RecipeDetailView";
 
 export const MainPage = () => {
   return (
     <div>
       <Title title="Fresh & New" />
+      {/* <RecipeDetailView /> */}
       <FreshAndNew />
       <Title title="Most Popular Recipes" />
     </div>

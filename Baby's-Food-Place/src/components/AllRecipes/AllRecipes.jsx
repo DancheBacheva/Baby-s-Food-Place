e61b091d0e851b800a898b233d1f37c988aa1896
DancheBacheva <danchebacheva@gmail.com>
2024-11-10 21:16:45 +0100

    Resolved image path issue, Implemented filtering of recipes based on type, 'Create Account' button in place

--- Baby's-Food-Place/src/components/AllRecipes/AllRecipes.jsx
+++ Baby's-Food-Place/src/components/AllRecipes/AllRecipes.jsx
@@ -1,42 +1,55 @@
 import React from "react";
 import styles from "./AllRecipes.module.css";
 import data from "../../data/recipes.json";
+import { useParams } from "react-router-dom";
 
 export const AllRecipes = () => {
+  const { type } = useParams();
+  const filteredRecipes = data.filter(
+    (recipe) => recipe.category.toLowerCase() === type.toLowerCase()
+  );
   return (
     <>
       <div className={styles.mainContainer}>
-        {data.map((recipe, i) => (
-          <div key={i} className={styles.card}>
-            <div className={styles.cardUp}>
-              <img src={recipe.recipeImage} alt="" />
-              <div className={styles.foodCategory}>{recipe.category}</div>
-            </div>
-            <div className={styles.cardDown}>
-              <h4>{recipe.recipeTitle}</h4>
-              <p>{recipe.recipeDescription}</p>
-              <div className={styles.detailsContainer}>
-                <div className={styles.detailsContainerLeft}>
-                  <div className={styles.smallContainer}>
-                    <img src="clock.png" alt="" />
-                    <p>{recipe.preparationTime}min</p>
+        {filteredRecipes.length > 0 ? (
+          filteredRecipes.map((recipe, i) => (
+            <div key={i} className={styles.card}>
+              <div className={styles.cardUp}>
+                <img
+                  src={`/food/${recipe.recipeImage}`}
+                  alt={recipe.recipeTitle}
+                />
+
+                <div className={styles.foodCategory}>{recipe.category}</div>
+              </div>
+              <div className={styles.cardDown}>
+                <h4>{recipe.recipeTitle}</h4>
+                <p>{recipe.recipeDescription}</p>
+                <div className={styles.detailsContainer}>
+                  <div className={styles.detailsContainerLeft}>
+                    <div className={styles.smallContainer}>
+                      <img src="clock.png" alt="" />
+                      <p>{recipe.preparationTime}min</p>
+                    </div>
+                    <div className={styles.smallContainer}>
+                      <img src="cutlery.png" alt="" />
+                      <p>{recipe.numberOfPeople} persons</p>
+                    </div>
+                    <div className={styles.smallContainer}>
+                      <img src="star.png" alt="" />
+                      <p>28</p>
+                    </div>
                   </div>
-                  <div className={styles.smallContainer}>
-                    <img src="cutlery.png" alt="" />
-                    <p>{recipe.numberOfPeople} persons</p>
+                  <div className={styles.detailsContainerRight}>
+                    <button>&gt;&gt;</button>
                   </div>
-                  <div className={styles.smallContainer}>
-                    <img src="star.png" alt="" />
-                    <p>28</p>
-                  </div>
-                </div>
-                <div className={styles.detailsContainerRight}>
-                  <button>&gt;&gt;</button>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>No recipes found for this category.</p>
+        )}
       </div>
     </>
   );
